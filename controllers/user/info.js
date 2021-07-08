f0febb6ae1b2b9953cb2795fbe4d34d5ca740e7c
SundeulDonaKim <kphddn@gmail.com>
2021-07-08 17:28:12 +0900

    #13:edit:logout

--- controllers/user/info.js
+++ controllers/user/info.js
@@ -1,37 +1,28 @@
 const { users } = require("../../mongodb/models");
-const { isAuthorized } = require("../tokenHandle");
 
 module.exports = async (req, res) => {
-  const token = isAuthorized(req);
-  if(!token) {
-    res.status(401).send({
-      data: null,
-	    message: "Authorization dont exist"
+  let user = await users.findOne({ _id: token.id });
+  if(user){
+    user = {
+      id: user._id,
+      ...user._doc,
+      password: undefined,
+      createdAt: undefined,
+      _id: undefined,
+      __v: undefined
+    }
+    res.status(200).send({
+      data: {
+        userInfo: user
+      },
+      message: "ok"
     });
   }else{
-    let user = await users.findOne({ _id: token.id });
-    if(user){
-      user = {
-        id: user._id,
-        ...user._doc,
-        password: undefined,
-        createdAt: undefined,
-        _id: undefined,
-        __v: undefined
-      }
-      res.status(200).send({
-        data: {
-          userInfo: user
-        },
-        message: "ok"
-      });
-    }else{
-      res.status(200).send({
-        data: {
-          userInfo: null
-        },
-        message: "force logout"
-      });
-    }
+    res.status(200).send({
+      data: {
+        userInfo: null
+      },
+      message: "force logout"
+    });
   }
 };
--- controllers/user/logout.js
+++ controllers/user/logout.js
@@ -1,18 +1,6 @@
-const { isAuthorized } = require("../tokenHandle");
-
 module.exports = async (req, res) => {
-  const token = isAuthorized(req);
-  if(!token) {
-    res.status(205).send({
-      data: null,
-	    message: "You are currently not logined but successfully signed out"
-    });
-  }else{
-    res.status(205).cookie("refreshToken", "invalidtoken").send({
-      data: {
-        id: token.id
-      },
-      message: "Successfully signed out"
-    });
-  }
+  res.status(205).cookie("refreshToken", "invalidtoken").send({
+    data: null,
+    message: "Successfully signed out"
+  });
 };
