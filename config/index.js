185a072c863f59b2700d7a399131cb37faa53e67
lhb7021 <manevil6@gmail.com>
2021-07-11 18:18:35 +0900

    edit:userDelete:allData

--- config/index.js
+++ config/index.js
@@ -2,22 +2,21 @@ require("dotenv").config()
 
 let username, password, dbName, dbUrl, redirectUri, domain;
 
-// if(process.env.NODE_ENV === "production"){
+if(process.env.NODE_ENV === "production"){
   redirectUri = process.env.REDIRECT_URI;
   username = process.env.MONGO_DB_USERNAME;
   password = process.env.MONGO_DB_PASSWORD;
   dbName = process.env.MONGO_DB_NAME;
   dbUrl = process.env.MONGO_DB_URL;
+  domain = "actorz.click"
+}else{
+  redirectUri = process.env.REDIRECT_URI_TEST;
+  username = process.env.TESTING_DB_USERNAME;
+  password = process.env.TESTING_DB_PASSWORD;
+  dbName = process.env.TESTING_DB_NAME;
+  dbUrl = process.env.TESTING_DB_URL;  
   domain = "localhost:3000"
-//   domain = "actorz.click"
-// }else{
-//   redirectUri = process.env.REDIRECT_URI_TEST;
-//   username = process.env.TESTING_DB_USERNAME;
-//   password = process.env.TESTING_DB_PASSWORD;
-//   dbName = process.env.TESTING_DB_NAME;
-//   dbUrl = process.env.TESTING_DB_URL;  
-//   domain = "localhost:3000"
-// }
+}
 
 module.exports = {
   domain,
